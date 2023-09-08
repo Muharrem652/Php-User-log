d3b3b8d3dcab996704e26a24ef0baa826a03650b
Muharrem <101968753+Muharrem652@users.noreply.github.com>
2023-09-08 17:07:31 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Ekranı</title>
+</head>
+<body>
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+        <h1>Test</h1>
+    </div>
+</body>
+</html>
+<?php
+require 'izleme.php';
+?>
